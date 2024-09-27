0657706c625ea9dd67304cd3cd5b7a778296bb54
Dylan Carter <carterdw@hendrix.edu>
2024-09-26 22:15:18 -0500

    Fixed errors and reversed direction of compass

--- compix/lib/main.dart
+++ compix/lib/main.dart
@@ -167,7 +167,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             const SizedBox(height: 50),
             AnimatedRotation(
-                  turns: turns,
+                  turns: - turns,
                   duration: const Duration(milliseconds: 250),
                   child:Image.asset("lib/asset/compass.png",fit: BoxFit.fill,)
                 ),
